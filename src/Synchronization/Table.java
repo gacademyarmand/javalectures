18769103fc07450a36f0b30eab4f5932f9b1c832
gacademyarmand <gacademyarmand@gmail.com>
2024-08-05 13:55:39 +0100

    Synchronized block

--- src/Synchronization/Table.java
+++ src/Synchronization/Table.java
@@ -1,21 +1,21 @@
 package Synchronization;
 
 class Table{
-    synchronized void printTable(int n){//method not synchronized
+    void printTable(int n){//method not synchronized
         System.out.println("thread -- "+Thread.currentThread().getName()+" has entered the bank");
+
+        synchronized (this) {
             System.out.println("thread -- "+Thread.currentThread().getName()+" has entered the ATM");
-
-
-            for(int i=1;i<=5;i++){
-                System.out.println(n*i);
-                try{
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(n * i);
+                try {
                     Thread.sleep(400);
-                }catch(Exception e){System.out.println(e);}
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
             }
-
+        }
 
         System.out.println("thread -- "+Thread.currentThread().getName()+" has left the ATM");
-
-
     }
 }
