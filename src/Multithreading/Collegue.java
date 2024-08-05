561e1aa3542d923a9e0b0ee99cd9d9aeb6f2ba6b
gacademyarmand <gacademyarmand@gmail.com>
2024-08-05 13:32:25 +0100

    Thread joining

---
+++ src/Multithreading/Collegue.java
@@ -0,0 +1,24 @@
+package Multithreading;
+
+public class Collegue extends Thread {
+
+    public Collegue(String threadName) {
+        super(threadName);
+    }
+
+    public Collegue() {}
+
+    @Override
+    public void run() {
+        super.run();
+        for(int i=0;i < 5 ;i++) {
+            try {
+                Thread.sleep(300);
+                System.out.println(Thread.currentThread().getName()+ i);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/Multithreading/DemoJoin.java
@@ -0,0 +1,27 @@
+package Multithreading;
+
+public class DemoJoin {
+
+    public static void main(String[] args) {
+        Collegue collegue1 = new Collegue();
+        collegue1.setName("ameri");
+        Collegue collegue2 = new Collegue();
+        collegue2.setName("jeorge");
+        Collegue collegue3 = new Collegue("martins");
+
+        collegue1.start();
+
+        try {
+            collegue1.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        collegue2.start();
+        collegue3.start();
+
+
+    }
+
+}
